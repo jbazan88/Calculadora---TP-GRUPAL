07e27f706687426c5f15e640e146c7c3962fd3b4
jbazan88 <juan.bazan88@yahoo.com>
2024-10-25 20:43:07 -0300

    Corrección del main

---
+++ main.js
@@ -0,0 +1,9 @@
+const sumar = require('./sumar');
+const restar = require('./restar');
+const multiplicar = require('./multiplicar');
+const dividir = require('./dividir');
+
+console.log(sumar());
+console.log(restar()); 
+console.log(multiplicar());
+console.log(dividir());
